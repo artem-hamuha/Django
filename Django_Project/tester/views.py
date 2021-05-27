4c6c809592182a49442a13ddf26a2f1f60c78f8a
Artem <artem.hamuha@gmail.com>
2021-05-26 20:39:20 -0700

    adding on to yesterdays

--- Django_Project/tester/views.py
+++ Django_Project/tester/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 # Create your views here.
 
-def index(reponse, id):
-    return HttpResponse ("<h1>%s</h1>" % id)
+def index(reponse, name):
+    ls = ToDoList.objects.get(name=name)
+    return HttpResponse ("<h1>%s</h1>" % ls.name)
