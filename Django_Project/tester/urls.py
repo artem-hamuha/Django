1f8837e7e0fd4758dc76587796b129b80a8378b9
Artem <artem.hamuha@gmail.com>
2021-06-08 18:40:36 -0700

    finally fixed the problem. new page creates to do lists and then takes you to their page

--- Django_Project/tester/urls.py
+++ Django_Project/tester/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path("<int:id>", views.index, name="index"),
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("create/", views.create, name="index"),
+    path("anton/", views.antons, name="index"),
 ]
 
 
--- Django_Project/tester/views.py
+++ Django_Project/tester/views.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import ToDoList, Item
 from .forms import CreateNewList
 # Create your views here.
@@ -12,14 +12,21 @@ def index(response, id):
 def home(response):
     return render(response, "tester/home.html", {})
 
+def antons(response):
+    return render(response, "tester/anton.html", {})
+
 def create(response):
     if response.method == "POST":
         form = CreateNewList(response.POST)
-        
+
         if form.is_valid():
             n = form.cleaned_data["name"]
             t = ToDoList(name=n)
             t.save()
+
+        return HttpResponseRedirect("/%i" %t.id)
+
     else:
         form = CreateNewList()
-        return render(response, "tester/create.html", {"form":form})
+
+    return render(response, "tester/create.html", {"form":form})
