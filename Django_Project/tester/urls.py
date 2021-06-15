d9a2a70c6af550d122ca4849fc0980a3f2f03591
Artem <artem.hamuha@gmail.com>
2021-06-14 18:31:42 -0700

    adding bootstrap

--- Django_Project/tester/urls.py
+++ Django_Project/tester/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("<int:id>", views.index, name="index"),
-    path("home", views.home, name="home"),
+    path("", views.home, name="home"),
     path("create/", views.create, name="index"),
     path("anton/", views.antons, name="index"),
     
